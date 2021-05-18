578df12c08d88868393c77ebb0bd6beb1f363b02
Gauri-g <gaurigupta2727@gmail.com>
2021-05-18 17:32:25 +0530

    feat: First Section completed

--- src/sections/section1.js
+++ src/sections/section1.js
@@ -1,11 +1,34 @@
 import Navbar from "../components/section1/Navbar";
 import "./section1.css";
+import img from "../images/sec1.png";
 
 const section1 = () => {
     return(
         <>
         <div className="section1">
-        <Navbar />
+            <Navbar />
+            <div className="container heading">
+                <h1 className="bigtext">Automate your time tracking </h1>
+                <h1 className="bigtext">and forget about the timer.<span className="highlighted"> Forever.</span> </h1>
+            </div>
+            <div className="container btns" >
+                <div className="download">
+                    <button className="btn1" href="#otherresources">
+                    Download Free Trial
+                    </button>
+                    <div className="footnote">
+                        Version 2.7.4 ∙ Release Notes
+                    </div>
+                </div>
+                <div className="download">
+                <button className=" btn2" href="#otherresources">
+                  Watch Video
+                </button>
+                </div>
+            </div>
+            <div className="imgmain">
+                <img src={img} alt="image" />
+            </div>
         </div>
         </>
     )
